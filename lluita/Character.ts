29da223504d48c21cdaed13a2d51992bab9fdc74
Eloi Gomez <egomezi962@boscdelacoma.cat>
2026-04-30 13:46:12 +0200

    poder

--- lluita/Character.ts
+++ lluita/Character.ts
@@ -1,14 +1,14 @@
 export type nom_personatge = "Humano" | "Elfo" | "Enano" | "Orco" | "Goblin";
-
+export type poder_personatges = 1 | 2 | 3 | 4 | 5 | 10;
 
 export class Character {
     private _name: nom_personatge;
-    private _health: number;
-    private _attack: number;
-    private _defense: number;
+    private _health: poder_personatges;
+    private _attack: poder_personatges;
+    private _defense: poder_personatges;
 
 
-    constructor(name: nom_personatge, health: number, attack: number, defense: number) {
+    constructor(name: nom_personatge, health: poder_personatges, attack: poder_personatges, defense: poder_personatges) {
         this._name = name;
         this._health = health;
         this._attack = attack;

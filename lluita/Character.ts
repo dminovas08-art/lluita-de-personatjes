a7959af71a5097db0f69cd7076fe109521205d66
david minovas <dminovas08@boscdelacoma.cat>
2026-04-30 13:40:40 +0200

    nom caracters

--- lluita/Character.ts
+++ lluita/Character.ts
@@ -1,13 +1,18 @@
+export type nom_personatge = "Humano" | "Elfo" | "Enano" | "Orco" | "Goblin";
+
+
 export class Character {
-    private _name: string;
+    private _name: nom_personatge;
     private _health: number;
     private _attack: number;
     private _defense: number;
 
-    constructor(name: string, health: number, attack: number, defense: number) {
+
+    constructor(name: nom_personatge, health: number, attack: number, defense: number) {
         this._name = name;
         this._health = health;
         this._attack = attack;
         this._defense = defense;
+
     }
 }

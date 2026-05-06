4eeb2826c19322e387b528da926d522586126dbe
Eloi Gomez <egomezi962@boscdelacoma.cat>
2026-05-06 09:22:03 +0200

    Canvi charcters

--- lluita/Character.ts
+++ lluita/Character.ts
@@ -3,7 +3,7 @@ export type poder_personatges = 1 | 2 | 3 | 4 | 5 | 10;
 
 export class Character {
     private _name: nom_personatge;
-    private _health: poder_personatges;
+    private _health: number;
     private _attack: poder_personatges;
     private _defense: poder_personatges;
 
@@ -15,4 +15,25 @@ export class Character {
         this._defense = defense;
 
     }
+
+    get name(): nom_personatge {
+        return this._name;
+    }
+
+    get health(): number {
+        return this._health;
+    }
+
+    get attack(): poder_personatges {
+        return this._attack;
+    }
+
+    get defense(): poder_personatges {
+        return this._defense;
+    }
+
+    public comprovarVida(): boolean {
+        return this._health > 0;
+    }
+
 }

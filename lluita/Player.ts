211f48387dc4eb30b958562814e088b6d8986370
david minovas <dminovas08@boscdelacoma.cat>
2026-05-06 10:19:56 +0200

    modificacio view

--- lluita/Player.ts
+++ lluita/Player.ts
@@ -2,11 +2,27 @@ import { Team } from "./Team.ts";
 
 
 export class Player {
+    private _name: String;
     private _team: Team;
-    constructor(team: Team) {
+
+    constructor(team: Team, name: String) {
         this._team = team;
+        this._name = name;
     }
+
+    get name(): String {
+        return this._name;
+    }
+
+    set name(newName: String) {
+        this._name = newName;
+    }
+
     get team(): Team {
         return this._team;
+    }
+
+    public removeTeam(): void {
+        this._team.remove();
     }
 }
--- lluita/View.ts
+++ lluita/View.ts
@@ -1,3 +1,31 @@
 import type { Game } from "./Game";
 import type { Team } from "./Team";
 import type { Character } from "./Character";
+
+export class View {
+    private _j1Name: HTMLDivElement;
+    private _j1Heal: HTMLDivElement;
+    private _j1Attack: HTMLDivElement;
+    private _j1Defens: HTMLDivElement;
+    private _j2Name: HTMLDivElement;
+    private _j2Heal: HTMLDivElement;
+    private _j2Attack: HTMLDivElement;
+    private _j2Defens: HTMLDivElement;
+
+    constructor() {
+        this._j1Name = document.getElementById("j1-name") as HTMLDivElement;
+        this._j1Heal = document.getElementById("j1-hp") as HTMLDivElement;
+        this._j1Attack = document.getElementById("j1-attack") as HTMLDivElement;
+        this._j1Defens = document.getElementById("j1-defense") as HTMLDivElement;
+        this._j2Name = document.getElementById("j2-name") as HTMLDivElement;
+        this._j2Heal = document.getElementById("j2-hp") as HTMLDivElement;
+        this._j2Attack = document.getElementById("j2-attack") as HTMLDivElement;
+        this._j2Defens = document.getElementById("j2-defense") as HTMLDivElement;
+    }
+
+    public render(game: Game): void {
+
+    }
+
+
+}

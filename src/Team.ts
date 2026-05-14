6dff4d78188656a02492b09a265122c08b450e3b
david minovas <dminovas08@boscdelacoma.cat>
2026-05-14 11:54:56 +0200

    mes numeros

--- src/Team.ts
+++ src/Team.ts
@@ -1,6 +1,6 @@
 import { Character, type nom_personatge, type poder_personatges } from "./Character";
 const NOM_PERSONATGE: nom_personatge[] = ["Humano", "Elfo", "Enano", "Orco", "Goblin", "Pato"];
-const PODER_PERSONATGES: poder_personatges[] = [1, 2, 3, 4, 5, 6, 10]
+const PODER_PERSONATGES: poder_personatges[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 export class Team {
 
@@ -14,12 +14,15 @@ export class Team {
 
     public generarPersonatges(): void {
         for (const nom of NOM_PERSONATGE) {
-            const poderAleatori = PODER_PERSONATGES[Math.floor(Math.random() * PODER_PERSONATGES.length)];
-            const personatge = new Character(nom, poderAleatori, poderAleatori, poderAleatori);
+            const _vida = PODER_PERSONATGES[Math.floor(Math.random() * PODER_PERSONATGES.length)];
+            const _attack = PODER_PERSONATGES[Math.floor(Math.random() * PODER_PERSONATGES.length)];
+            const _defense = PODER_PERSONATGES[Math.floor(Math.random() * PODER_PERSONATGES.length)];
+
+            const personatge = new Character(nom, _vida, _attack, _defense);
 
             this._characters.push(personatge);
 
-            
+
         }
     }
 

02f3b77a55974f80690fd5e71aad332eaf8127ee
david minovas <dminovas08@boscdelacoma.cat>
2026-05-20 10:56:56 +0200

    per fi ja no son pacificstes

--- src/Team.ts
+++ src/Team.ts
@@ -19,7 +19,7 @@ export class Team {
             const _attack = PODER_PERSONATGES[Math.floor(Math.random() * PODER_PERSONATGES.length)];
             const _defense = PODER_PERSONATGES[Math.floor(Math.random() * PODER_PERSONATGES.length)];
 
-            const personatge = new Character(nom, _vida, _attack, _defense);        
+            const personatge = new Character(nom, _vida, _attack, _defense);
             this._characters.push(personatge);
 
 
@@ -49,7 +49,7 @@ export class Team {
     public push(character: Character): void {
         this._characters.push(character);
     }
-    public pop(): Character | undefined {
-        return this._characters.pop();
+    public shift(): Character | undefined {
+        return this._characters.shift();
     }
 }
